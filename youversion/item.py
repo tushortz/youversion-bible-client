0a6eaf9f0d95b0005f3d4366bdbfe543cbfe85c5
Taiwo Kareem <taiwo.kareem36@gmail.com>
2023-06-04 06:34:38 +0100

    fixed bugs
    updated code for Moment object
    
    created highlight class

--- youversion/item.py
+++ youversion/item.py
@@ -1,7 +1,8 @@
 import dataclasses as dc
-from typing import List, Optional
+from typing import Dict, List, Optional
 
 from dateutil import parser
+from youversion._endpoints import HOME
 
 
 @dc.dataclass
@@ -37,6 +38,16 @@ class Image(Base):
 
 
 @dc.dataclass
+class Reference(Base):
+    """Reference class for Youversion moment objects
+    """
+
+    version_id: str
+    human: str
+    usfm: List[str]
+
+
+@dc.dataclass
 class Moment(Base):
     """Base class for Youversion moment objects
     """
@@ -44,14 +55,16 @@ class Moment(Base):
     id: str
     kind: str
     moment_title: str
-    body_images: Optional[List[Image]] = None
-    action_url: str
     created_dt: str
     updated_dt: str
     path: str
     avatar: str
     time_ago: str
-    owned_by_me: str
+    owned_by_me: bool
+    actions: Optional[Dict]
+    body_images: Optional[List[Image]] = None
+    action_url: Optional[str] = None
+    references: Optional[List[Reference]] = None
 
     @property
     def created_date(self):
@@ -72,3 +85,30 @@ class Moment(Base):
         """
         if self.updated_dt:
             return parser.parse(self.updated_dt)
+
+    @property
+    def moment_url(self):
+        """Generates the full url to the moment object
+
+        Returns:
+            str: the full url to the moment object
+        """
+        if self.path:
+            return f"{HOME}{self.path}"
+
+
+@dc.dataclass
+class Highlight(Moment):
+    """Highlight class for the Youversion moment object
+    """
+    pass
+
+
+@dc.dataclass
+class Action(Base):
+    """Action class for the Youversion moment object
+    """
+    show: bool = False
+    editable: bool = False
+    deletable: bool = True
+    read: bool = True
